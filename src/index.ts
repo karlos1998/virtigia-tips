40317eb5a0a5bfc068906ddb1edd98b5f1ced1b3
karlos1998 <fadeusz98@gmail.com>
2026-01-03 23:35:14 +0100

    Revert "feat: Add createItemTip utility function"
    This reverts commit 76c706f1c1dd9ade8ae0bae070f124e43e81faff.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import ToolTipDirective, { useToolTip } from "./tooltips/module";
 import RockTip from "./RockTip/components/rockTip.vue";
 import type {HtmlPayload, ItemPayload, NpcPayload, OtherPayload} from './RockTip/typings/payloads';
-import { createItemTip } from './RockTip/utilities/itemTipGenerator';
 
-export { ToolTipDirective, RockTip, useToolTip, HtmlPayload, ItemPayload, NpcPayload, OtherPayload, createItemTip };
+export { ToolTipDirective, RockTip, useToolTip, HtmlPayload, ItemPayload, NpcPayload, OtherPayload };
